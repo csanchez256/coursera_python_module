f18ec764ad8cbb5a0a526a1bea7f12de36045918
chris <css7c3@gmail.com>
2024-08-03 11:53:16 -0600

    reading in a file

--- whileLoops.py
+++ whileLoops.py
@@ -1,6 +1,6 @@
-# Example of readin in a .txt file line by line with a for loop
+# Example of reading in a .txt file line by line with a for loop
 
-with open('zen_of_python.txt') as f:
+with open('C:\\Users\\css7c\\OneDrive\\Desktop\\Coursera Data Analytics Certificate\\COURSE 2 - PYTHON PROGRAMMING\\zen_of_python.txt') as f:
     for line in f:
         print(line)
 print('\nI\'m done.')
